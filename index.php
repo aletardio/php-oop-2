774a2ac0abc7977975d5f83baa6559503152bf6a
aletardio <ale.tardio99@gmail.com>
2024-01-31 17:04:37 +0100

    created file style.css

--- index.php
+++ index.php
@@ -85,15 +85,18 @@
                     $product_7, 
                     $product_8
                 ];
-
-    foreach($food as $product){
-        echo $product->getInfoFood()."<br><br>";
-    }
-    foreach($toys as $product){
-        echo $product->getInfoToy()."<br><br>";
-    }
-    foreach($house as $product){
-        echo $product->getInfoHouse()."<br><br>";
-    }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>OOP Shop</title>
+</head>
+<body>
+    
+</body>
+</html>
